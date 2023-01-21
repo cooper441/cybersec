e125ffd3c1967310480d7732a4537d54c2de4555
Tom Cooper <cooper.tom.201@gmail.com>
2023-01-21 12:31:51 +0000

    test commint

---
+++ src/SocketExampleClient.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+class SocketExampleClient {
+
+    public static void main(String [] args) throws Exception {
+
+        String host = "localhost"; // hostname of server
+        int port = 5678;           // port of server
+
+        try {
+            Socket s = new Socket(host, port);
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            dos.writeUTF("Hello World!");
+            dos.writeUTF("Happy new year!");
+
+        } catch (Exception e) {
+            System.err.println("Cannot connect to server.");
+        }
+
+    }
+}
---
+++ src/SocketExampleServer.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+import java.io.*;
+import java.net.*;
+class SocketExampleServer {
+
+    public static void main(String[] args) throws Exception {
+        int port = 5678;
+        ServerSocket ss = new ServerSocket(port);
+
+        while (true) {
+            System.out.println("Waiting incoming connection...");
+
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+
+            String x = null;
+
+            try {
+                while ((x = dis.readUTF()) != null) {
+
+//                System.out.print(x.)
+                    System.out.println(x);
+
+                }
+            } catch (IOException e) {
+                System.err.println("Client closed its connection.");
+            }
+        }
+    }
+}
+
+
+
